efbeed57af96b0efea39a552a13231e7ad6c4317
TanWang-0914 <wang.tan@northeastern.edu>
2021-04-01 10:16:34 -0700

    assignment-3 20210401

--- PurchasesDBServer/src/main/java/DynamoDBSource.java
+++ PurchasesDBServer/src/main/java/DynamoDBSource.java
@@ -19,16 +19,16 @@ public class DynamoDBSource {
 
 
     static {
-        BasicSessionCredentials sessionCredentials = new BasicSessionCredentials(
-                "ASIAYTP4QQCGFE32ESI5",
-                "UGdnmzgYDdhmQOYpzs1hPs6cVpbQntZlJNCvwSRq",
-                "FwoGZXIvYXdzEP7//////////wEaDAheTaH1wrJ2+c9siCLIAcSHKrGplkWCPBLNCJdAhVj1ve5a+Ufgddhv+LB8ihKPwpYt7uDOfSsVI2A89WeGxBRe+IXB5jH8YDNJ8bhgVzmSXI6NqVb+KxuGNjGqanAD1kzYnEtOwv/78JezIuCAOe0fjdh+5m8dO0/3rMNjbLveGOTcZtag79E0v2N3/7igY6Dv+OoVuS2ZUqzITme/TSLD3SG1i8n2hX7eOTyE4OW6eMiyy9w7x3P/MYcW+wSzn0NQPXI+SlU/f1NND9vG/TA82mBX73/xKLay/oIGMi2JroDS5JwtSMicg0yHeNyCLT38i/Pa4KaAJK6SCOftYWvShr0cPy+52F0zW+g="
-        );
 //        BasicSessionCredentials sessionCredentials = new BasicSessionCredentials(
-//                awsAccessKey,
-//                awsSecretKey,
-//                awsSessionToken
+//                "ASIAYTP4QQCGFE32ESI5",
+//                "UGdnmzgYDdhmQOYpzs1hPs6cVpbQntZlJNCvwSRq",
+//                "FwoGZXIvYXdzEP7//////////wEaDAheTaH1wrJ2+c9siCLIAcSHKrGplkWCPBLNCJdAhVj1ve5a+Ufgddhv+LB8ihKPwpYt7uDOfSsVI2A89WeGxBRe+IXB5jH8YDNJ8bhgVzmSXI6NqVb+KxuGNjGqanAD1kzYnEtOwv/78JezIuCAOe0fjdh+5m8dO0/3rMNjbLveGOTcZtag79E0v2N3/7igY6Dv+OoVuS2ZUqzITme/TSLD3SG1i8n2hX7eOTyE4OW6eMiyy9w7x3P/MYcW+wSzn0NQPXI+SlU/f1NND9vG/TA82mBX73/xKLay/oIGMi2JroDS5JwtSMicg0yHeNyCLT38i/Pa4KaAJK6SCOftYWvShr0cPy+52F0zW+g="
 //        );
+        BasicSessionCredentials sessionCredentials = new BasicSessionCredentials(
+                awsAccessKey,
+                awsSecretKey,
+                awsSessionToken
+        );
 
         client = AmazonDynamoDBClientBuilder.standard()
                 .withCredentials(new AWSStaticCredentialsProvider(sessionCredentials))
